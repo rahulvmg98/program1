942dc9a99c6c8cfe61ad85beeb0135520a3beb93
rahulvmg98 <47689124+rahulvmg98@users.noreply.github.com>
2019-06-24 12:45:41 +0530

    Update maximl.py

--- maximl.py
+++ maximl.py
@@ -1,7 +1,13 @@
 String=input()
 result=''
+count=0
+maxi=[]
 for i in range(len(String)):
     if(not String[i] in result):
         result =result+String[i]
-print(len(result))
-        
+        count+=1
+    else:
+        result=String[i]
+        maxi.append(count)
+        count=1
+print(max(maxi))
